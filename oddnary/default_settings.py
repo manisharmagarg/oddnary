4c08e109e7b1b140a3caba72979d71fdcf35257a
Manish <sharmagarg007@gmail.com>
2021-03-12 10:54:44 +0530

    SECRET_KEY

--- oddnary/default_settings.py
+++ oddnary/default_settings.py
@@ -29,7 +29,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ')i4ptecwosha1rjk!ddxhzb87va8b7r=9e9nxl@*08*ziw4@)$'
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
